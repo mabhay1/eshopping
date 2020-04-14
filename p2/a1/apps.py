34779736c4504c86a5331caccbf8b1b031c3c0a2
abhay mendiratta <mabhay1994@gmail.com>
2020-04-14 12:38:00 +0530

    next commit

--- p2/a1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class A1Config(AppConfig):
-    name = 'a1'
--- p2/a1/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import UserProfile,CartVariable
-def func1(request):
-    try:
-        user_profile=UserProfile.objects.get(username=request.user.username)
-        cart7=CartVariable.objects.get_or_create(user_id=user_profile.user_ptr_id)[0]
-        return {'qty':cart7.total_qty}
-    except:
-        return {'qty':0}    
